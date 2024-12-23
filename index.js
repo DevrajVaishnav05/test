3ece7e51332cf02b1ad463f7b6b7a5039719b0f3
Devraj Vaishnav <devrajv2023@gmail.com>
2024-12-23 21:24:31 +0530

    error solve 1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 
 // Define the route for the home page
 app.get('/', (req, res) => {
-  res.send('<h1>Hello, this is a simple page hosted on Vercel!</h1>');
+  res.render('index');
 });
 
 // Start the server
